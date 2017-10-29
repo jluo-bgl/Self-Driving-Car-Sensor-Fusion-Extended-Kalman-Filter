c8fb7f03a24e416dca0994d86742999961de3186
James Luo <jluo@bglcorp.com.au>
2017-10-29 16:27:13 +1100

    rader done

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -155,6 +155,10 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.R_ = R_radar_;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
     ekf_.H_ = H_laser_;
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -58,4 +58,46 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   */
 
   //Radar
+  double px = x_[0];
+  double py = x_[1];
+  double vx = x_[2];
+  double vy = x_[3];
+
+  if(fabs(px) < 0.0001){
+    px = 0.0001;
+  }
+
+  double rho = sqrt(px*px + py*py);
+  if(fabs(rho) < 0.0001){
+    rho = 0.0001;
+  }
+
+  double phi = atan2(py,px);
+  double rho_dot = (px*vx + py*vy)/rho;
+
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+
+  VectorXd y = z - z_pred;
+
+  //normalize angle between [-PI, PI]
+  while (y[1] > M_PI){
+    y[1]-=2*M_PI;
+  }
+
+  while (y[1] < -M_PI) {
+    y[1]+=2*M_PI;
+  }
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
